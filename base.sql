34c7c02e3710d4c580e04ca81b2c021f58545266
Anuar Tapia <anuartap@ciencias.unam.mx>
2016-02-20 21:20:11 -0600

    Crea script awk que genera número aleatorios

--- base.sql
+++ base.sql
@@ -187,3 +187,5 @@ ALTER TABLE `Tratar` ADD FOREIGN KEY (materia) REFERENCES `Materia` (`clave_mate
 -- ('','');
 -- INSERT INTO `Tratar` (`libro`,`ejemplar`,`materia`) VALUES
 -- ('','','');
+INSERT INTO `Libro` (`clave_libro`,`ejemplar`,`titulo`,`paginas`) VALUES
+('',,'',);
